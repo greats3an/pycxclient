c99464f897e813faff2a806d868cf4fa374924d0
greats3an <greats3an@gmail.com>
2020-04-07 12:54:36 +0800

    minor changes

--- apis/captchas/numericalcaptcha.py
+++ apis/captchas/numericalcaptcha.py
@@ -32,7 +32,7 @@ def RenewCaptcha(prompt=False) -> Union[bytearray,str]:
         userio.get('即将输入验证码，请记下您所看到的四位有效数字；按下回车查看验证码:',end='[确认]')
         # Deletes the old file
         showfile.tempname_length = 4
-        showfile.ShowBytes(captcha,ext='jpg',lifetime=5)
+        showfile.ShowBytes(captcha,ext='jpg',lifetime=10)
         captcha = userio.get('输入验证码')
     return captcha
 
--- imageviewer.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-copy "%cd%\temp\%~nx1" "F:\Global Shared\share\captcha\"
-echo 请在“验证码令牌认领”处输入该令牌:
-echo %~n1
